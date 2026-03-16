024cebbda5f388a453e182da92596ea75abcf40a
Santiago Jorda <santiagojorda@gmail.com>
2026-03-16 11:16:25 -0300

    script creacion de clientes

---
+++ generar-compose.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+IP_SUBNET="172.25.125.0/24"
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <OUTPUT_FILE> <AMOUNT_CLIENTS>"
+    exit 1
+fi
+
+OUTPUT_FILE=$1
+AMOUNT_CLIENTS=$2
+
+if ! [[ $AMOUNT_CLIENTS =~ ^[0-9][0-9]*$ ]]; then
+    echo "Error: Number of clients must be a positive integer."
+    exit 1
+fi
+
+# Server 
+cat > $OUTPUT_FILE <<EOL
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOL
+
+# Clients
+for i in $(seq 1 $AMOUNT_CLIENTS); do
+  cat >> $OUTPUT_FILE <<EOL
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOL
+
+done
+
+# Red
+cat >> $OUTPUT_FILE <<EOL
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: $IP_SUBNET
+EOL
