c5f35b670e6a51ed87522498f03688a204ea1261
Santiago Jorda <santiagojorda@gmail.com>
2026-03-16 12:50:28 -0300

    limpieza de los dockerfile para persistencia

--- generar-compose.sh
+++ generar-compose.sh
@@ -25,9 +25,10 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
 EOL
 
 # Clients
@@ -39,11 +40,12 @@ for i in $(seq 1 $AMOUNT_CLIENTS); do
     entrypoint: /client
     environment:
       - CLI_ID=$i
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     depends_on:
       - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
 EOL
 
 done
