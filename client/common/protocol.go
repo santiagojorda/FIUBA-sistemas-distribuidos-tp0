d838e8b00bafa51fdfb88796d8720b98435ba068
Santiago Jorda <santiagojorda@gmail.com>
2026-03-26 15:44:21 -0300

    actualizo protocolo, no envio la cantidad de bets en un inicio

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -13,6 +13,7 @@ const AGENCY_CSV_PATH_TEMPLATE = "/.data/agency-%s.csv"
 const MESSAGE_FIN = "FIN"
 const MESSAGE_OK = "ok"
 const MESSAGE_ERROR = "error"
+const MESSAGE_END_BATCH = "END_BATCH"
 const MESSAGE_ASK_WINNERS = "ASK_WINNERS"
 const MESSAGE_WAIT = "wait"
 
@@ -130,13 +131,13 @@ func (p *Protocol) SendBatch(bets []Bet) error {
 
 func (p *Protocol) serializeBatchPayload(bets []Bet) []byte {
 	var buffer bytes.Buffer
-	buffer.WriteString(strconv.Itoa(len(bets)))
-	buffer.WriteString("\n")
 
 	for _, bet := range bets {
 		formatBet(&buffer, bet)
 		buffer.WriteString("\n")
 	}
+	buffer.WriteString(MESSAGE_END_BATCH)
+	buffer.WriteString("\n")
 	return buffer.Bytes()
 }
 
