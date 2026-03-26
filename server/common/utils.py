e07dd5758beead6cbf61c2e04ece6787d6b670db
Santiago Jorda <santiagojorda@gmail.com>
2026-03-25 22:24:59 -0300

    refactorizo server/utils y quito la declaracion de bet

--- server/common/utils.py
+++ server/common/utils.py
@@ -1,28 +1,10 @@
 import csv
-import datetime
-import time
-
+from bet import Bet
 
 """ Bets storage location. """
 STORAGE_FILEPATH = "./bets.csv"
 """ Simulated winner number in the lottery contest. """
 LOTTERY_WINNER_NUMBER = 7574
-
-
-""" A lottery bet registry. """
-class Bet:
-    def __init__(self, agency: str, first_name: str, last_name: str, document: str, birthdate: str, number: str):
-        """
-        agency must be passed with integer format.
-        birthdate must be passed with format: 'YYYY-MM-DD'.
-        number must be passed with integer format.
-        """
-        self.agency = int(agency)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.document = document
-        self.birthdate = datetime.date.fromisoformat(birthdate)
-        self.number = int(number)
 
 """ Checks whether a bet won the prize or not. """
 def has_won(bet: Bet) -> bool:
